1f01cb0ff0e2475f0da91c087d0dc6f76c9aebb3
Rewazer <runner.px@gmail.com>
2020-06-17 23:20:52 +0500

    Add New Recipe Form and endpoint

--- src/main/java/ru/hcc/recipeapp/controllers/RecipeController.java
+++ src/main/java/ru/hcc/recipeapp/controllers/RecipeController.java
@@ -2,8 +2,11 @@ package ru.hcc.recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.hcc.recipeapp.commands.RecipeCommand;
 import ru.hcc.recipeapp.services.RecipeService;
 
 /*
@@ -23,5 +26,18 @@ public class RecipeController {
 
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
