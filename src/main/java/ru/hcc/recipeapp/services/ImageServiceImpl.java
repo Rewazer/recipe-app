afe6b96dbb4c59a2e800daa12dab10d6a2435df4
Rewazer <runner.px@gmail.com>
2020-06-26 19:58:41 +0500

    persist uploaded images in DB

--- src/main/java/ru/hcc/recipeapp/services/ImageServiceImpl.java
+++ src/main/java/ru/hcc/recipeapp/services/ImageServiceImpl.java
@@ -2,7 +2,12 @@ package ru.hcc.recipeapp.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import ru.hcc.recipeapp.domain.Recipe;
+import ru.hcc.recipeapp.repos.RecipeRepository;
+
+import java.io.IOException;
 
 /*
  * Created by SS on 26/06/2020.
@@ -12,9 +17,36 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class ImageServiceImpl implements ImageService {
 
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeService) {
+
+        this.recipeRepository = recipeService;
+    }
+
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
 
-        log.debug("Received a file");
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()) {
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            //todo handle better
+            log.error("Error occurred", e);
+
+            e.printStackTrace();
+        }
     }
 }
