4997d1af7585c57b26ad51cc039b7723297cbe81
Rewazer <runner.px@gmail.com>
2020-06-26 17:37:14 +0500

    add Delete Ingredient

--- src/main/java/ru/hcc/recipeapp/services/IngredientService.java
+++ src/main/java/ru/hcc/recipeapp/services/IngredientService.java
@@ -11,4 +11,6 @@ public interface IngredientService {
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long id);
 
     IngredientCommand saveIngredientCommand(IngredientCommand command);
+
+    void deleteById(Long recipeId, Long id);
 }
